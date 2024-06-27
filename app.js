dee3924d1843a1bdd45eecdad8512a4a3de9bf09
juz-bird <u6520150@au.edu>
2024-06-27 16:16:09 +0700

    add jquery

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+// console.log("hello world")
+
+L = [1, 3, 4, 5]
+
+// console.log(
+//     L.reduce((a, b) => a * b)
+// )
+
+L.sort((a, b) => b - a)
+
+console.log(L)
